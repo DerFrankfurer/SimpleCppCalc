e43a6115b6fae3557b2da472b7b0a1623692bdc2
Lars <167712462+DerFrankfurer@users.noreply.github.com>
2024-04-24 12:04:15 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# Simple C++ Calculator
+
+This is a simple cli based calculator implemented in C++ that allows performing basic arithmetic operations through the command line. The program takes a mathematical operation as a string, such as `3+4`, directly on the command line and displays the result in return.
+
+## Supported Operations
+The calculator supports the following operations:
+
+Addition (+)
+Subtraction (-)
+Multiplication (*)
+Division (/)
+
+## Prerequisites
+
+To run this program, you are going to need a C++ compiler. I used standard C++11, so any modern C++ compiler should work.
+
+## Compilation
+
+To compile the program, navigate to the directory where the source file is saved and run the following command:
+
+g++ -std=c++11 calculator.cpp -o calculator
+
---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <cctype> // Für isdigit
+#include <sstream> // Für stringstream
+
+int main() {
+    std::string input;
+    double num1, num2;
+    char operation;
+
+    std::cout << "Geben Sie eine Rechnung ein (z.B. 1+1): ";
+    std::getline(std::cin, input);
+
+    std::stringstream ss;
+    size_t i = 0;
+
+    while (i < input.size() && (isdigit(input[i]) || input[i] == '.')) {
+        i++;
+    }
+
+    operation = input[i];
+
+    ss << input.substr(0, i) << " " << input.substr(i + 1);
+    ss >> num1 >> num2;
+
+    switch(operation) {
+        case '+':
+            std::cout << "Ergebnis: " << num1 + num2 << std::endl;
+            break;
+        case '-':
+            std::cout << "Ergebnis: " << num1 - num2 << std::endl;
+            break;
+        case '*':
+            std::cout << "Ergebnis: " << num1 * num2 << std::endl;
+            break;
+        case '/':
+            if(num2 != 0.0) {
+                std::cout << "Ergebnis: " << num1 / num2 << std::endl;
+            } else {
+                std::cout << "Fehler: Division durch Null!" << std::endl;
+            }
+            break;
+        default:
+            std::cout << "Ungültige Operation!" << std::endl;
+    }
+
+    return 0;
+}
